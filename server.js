55ed6459340f90eb604ccf1a37d6f947b5ebed37
qiaofuhui <fuhui.qiao@gmail.com>
2013-02-26 17:51:10 +0800

    8888 -> 80

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ function start(route, handle) {
     route(handle, request, response);
   }
 
-  http.createServer(onRequest).listen(8888);
+  http.createServer(onRequest).listen(80);
   console.log("Server has started.");
 }
 
